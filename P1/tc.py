e75974da7d76c392d517b9342b8a3dfdfe9c92c3
MadTurt1e <81546749+MadTurt1e@users.noreply.github.com>
2024-10-12 00:14:14 -0400

    Updated to include laplace smoothing in denominator

--- P1/tc.py
+++ P1/tc.py
@@ -4,28 +4,21 @@ from collections import defaultdict
 
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords, wordnet
+from nltk.corpus import stopwords
 
 from tqdm import tqdm
 
 
 # I did Naive Bayes because it is straightforward to implement - "bag o words"
 class NaiveBayesClassifier:
-    def __init__(self):
-        # First, we set up the vocabulary as a "python array" with nothing
-        # (of course we don't do arrays in python, but let's say we do)
+    def __init__(self, smoothing_constant=0.015):
         self.vocab = set()
-
-        # We split the word counter into a double "array", where all values
-        # are initialized as 0 (by defaultdict)
         self.word_counts = defaultdict(lambda: defaultdict(int))
-        # We set up an "array" of all the categories, so we can figure out
-        # the documents spotted so far
-        self.category_counts = defaultdict(int)
-        # We also want to keep track of the total amount of documents seen
+        self.category_word_counts = defaultdict(int)
+        self.category_doc_counts = defaultdict(int)
         self.total_docs = 0
-        # Also there's a function that eliminates all "useless" words
         self.stop_words = set(stopwords.words("english"))
+        self.smoothing_constant = smoothing_constant
 
     def lemmatizer(self, word):
         # Use a lemmatizer because it may be more accurate
@@ -36,20 +29,6 @@ class NaiveBayesClassifier:
                 if token not in self.stop_words
                 and token not in string.punctuation]
 
-    # Synynoms can also be relevant
-    def get_synynoms(self, word):
-        synonyms = []
-        # Hunt through every synynom we find
-        for syn in wordnet.synsets(word):
-            for lemma in syn.lemmas():
-                # I notice synynoms have underscores for some reason
-                synonym = lemma.name().lower().replace("_", " ")
-                # If we have literally saw the synynom already ignore it
-                if synonym != word and synonym not in synonyms:
-                    synonyms.append(synonym)
-        # We now have a list of synynoms
-        return synonyms
-
     def train(self, documents, categories):
         # We record the amount of actual documents there are
         self.total_docs = len(documents)
@@ -58,54 +37,39 @@ class NaiveBayesClassifier:
         # I learned about tqdm in DL and now it is necessary everywhere
         for doc, category in tqdm(zip(documents, categories),
                                   total=len(documents), desc="  Training"):
-            # Mention to our NaiveBayes: hey, there's a document in here
-            self.category_counts[category] += 1
-            # We just lemmatize straight now
+            # Increase the count of documents of category
+            self.category_doc_counts[category] += 1
+            # sets avoid "double counting" in one document
             words = self.lemmatizer(doc)
 
-            # Iterate through every token
+            # Count the words
             for word in words:
-                # Note down that this is a word
                 self.vocab.add(word)
-
-                # Add this to (specifically) our category word count
                 self.word_counts[category][word] += 1
+                self.category_word_counts[category] += 1
 
-                # We can also add all the synynoms
-                synynoms = self.get_synynoms(word)
-                for synynom in synynoms:
-                    self.vocab.add(synynom)
-                    # Synynoms are slightly less meaningful, but I find they
-                    # should be meaningful depending on category count
-                    synynom_meaningfulness = 0.7
-                    if len(self.word_counts) < 4:
-                        synynom_meaningfulness = 0.2
-
-                    self.word_counts[
-                        category][synynom] += synynom_meaningfulness
-
-    # https://en.wikipedia.org/wiki/Naive_Bayes_classifier
-    # Naive Bayes is basically an implementation of Wikipedia
+    # Naive Bayes slideshow implementation instead
     def predict(self, document):
         words = self.lemmatizer(document)
         scores = {}
 
         # Calculate a probability score for our document
-        for category in self.category_counts:
+        for category in self.category_doc_counts:
             # Initialize the score as log probability of each category
-            score = math.log(self.category_counts[category] / self.total_docs)
+            score = math.log(self.category_doc_counts[category]
+                             / self.total_docs)
+
             # Go through every word
             for word in words:
-                # If the word exists in our vocabulary we find the prob of it
-                # appearing and add it to our bayes score
-                if word in self.vocab:
-                    word_count = self.word_counts[category][word]
-                    # Laplace smoothing from wikipedia
-                    score += math.log(
-                        (word_count + 1)
-                        / (sum(self.word_counts[category].values())
-                           + len(self.vocab))
-                    )
+                # Find prob of word appearing and add it to bayes score
+                word_count = self.word_counts[category][word]
+                total_words = self.category_word_counts[category]
+
+                # Improved probability estimation with Laplace smoothing
+                word_prob = (word_count + self.smoothing_constant) / (
+                    total_words + (len(self.vocab) * self.smoothing_constant)
+                )
+                score += math.log(word_prob)
             scores[category] = score
 
         # Return the category with the highest score
